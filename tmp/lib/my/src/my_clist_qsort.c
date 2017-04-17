2379d64fc8aef457815ee51b1a4c38f825861bb4
Viguier Benjamin <benjamin.viguier@epitech.eu>
2017-04-17 20:49:02 +0200

    quicksort optimisation :3

--- tmp/lib/my/src/my_clist_qsort.c
+++ tmp/lib/my/src/my_clist_qsort.c
@@ -5,18 +5,16 @@
 ** Login   <benjamin.viguier@epitech.eu>
 ** 
 ** Started on  Fri Apr 14 16:12:39 2017 Benjamin Viguier
-** Last update Mon Apr 17 20:03:12 2017 Benjamin Viguier
+** Last update Mon Apr 17 20:47:12 2017 Benjamin Viguier
 */
 
 #include "libmy.h"
 
-static void	__libmy_qsort_loop(t_clist_elm **pcur, t_clist_elm **pwall,
+static void	__libmy_qsort_loop(t_clist_elm *cur, t_clist_elm **pwall,
 				   t_clist_elm *pivot, t_elm_cmp cmp_fct)
 {
-  t_clist_elm	*cur;
   t_clist_elm	*wall;
 
-  cur = *pcur;
   wall = *pwall;
   while (1)
     {
@@ -31,8 +29,10 @@ static void	__libmy_qsort_loop(t_clist_elm **pcur, t_clist_elm **pwall,
       clist_swap_val(wall, cur);
     }
   *pwall = wall;
-  *pcur = cur;
 }
+
+#include <stdio.h>
+int             print_list(t_clist *list, t_clist *end);
 
 static void	__libmy_qsort(t_clist_elm *start, t_clist_elm *end,
 			      t_elm_cmp cmp_fct)
@@ -53,17 +53,30 @@ static void	__libmy_qsort(t_clist_elm *start, t_clist_elm *end,
       return;
     }
   pivot = start;
-  __libmy_qsort_loop(&cur, &wall, pivot, cmp_fct);
+  //print_list(start, cur);
+  //printf("\n");
+  
+  __libmy_qsort_loop(cur, &wall, pivot, cmp_fct);
   if ((res = cmp_fct(wall->ptr, pivot->ptr)) < 0)
     clist_swap_val(wall, pivot);
   cur = wall;
-  //while (cur->prev != start && !cmp_fct(cur->ptr, start->ptr))
-    //  cur = cur->prev;
+  
+  // printf("Pivot : %ld\n"
+	 //	 "Wall  : %ld\n", (long int) pivot->ptr, (long int) wall->ptr); 
+  //fflush(stdout);
+
+  while (cur != start && cur->prev != start && !cmp_fct(cur->prev->ptr, pivot->ptr))
+    cur = cur->prev;
+  
+  //print_list(start, cur);
+  //printf("\n\n");
+  //fflush(stdout);
+
   __libmy_qsort(start, cur, cmp_fct);
   if (res < 0)
     wall = wall->next;
-  //while (wall != end && !cmp_fct(wall->ptr, end->ptr))
-  //wall = wall->next;
+  while (wall != end && !cmp_fct(wall->ptr, pivot->ptr))
+    wall = wall->next;
   if (wall != end)
     __libmy_qsort(wall, end, cmp_fct);
 }
