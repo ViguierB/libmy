9cf1da649dea99b4db5b5ca18929b4b07917cafb
Viguier Benjamin <benjamin.viguier@epitech.eu>
2017-04-17 21:47:42 +0200

    warn lors de la compil pour lseek et open

--- tmp/lib/my/include/depend.mk
+++ tmp/lib/my/include/depend.mk
@@ -5,7 +5,7 @@
 ## Login   <benjamin.viguier@epitech.eu>
 ## 
 ## Started on  Wed Nov  9 17:16:44 2016 Benjamin Viguier
-## Last update Mon Apr 17 21:14:27 2017 Benjamin Viguier
+## Last update Mon Apr 17 21:47:20 2017 Benjamin Viguier
 ##
 
 CC		=	gcc
