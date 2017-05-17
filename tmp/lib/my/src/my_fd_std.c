d44a17b447ae696a63ee826adc0234dcbff157ff
Viguier Benjamin <benjamin.viguier@epitech.eu>
2017-05-18 01:40:18 +0200

    less malloc

--- tmp/lib/my/src/my_fd_std.c
+++ tmp/lib/my/src/my_fd_std.c
@@ -5,26 +5,23 @@
 ** Login   <benjamin.viguier@epitech.eu>
 ** 
 ** Started on  Thu Feb 23 18:01:04 2017 Benjamin Viguier
-** Last update Wed May 17 23:47:34 2017 Benjamin Viguier
+** Last update Thu May 18 01:39:31 2017 Benjamin Viguier
 */
 
 #include "libmy.h"
 #include "internal.h"
 
+static t_my_fd	g_my_fd_buffer[3];
 t_my_fd	*my_fdin;
 t_my_fd	*my_fdout;
 t_my_fd	*my_fderr;
 
 LMY_CONSTRUCTOR static void	__libmy_init_stdfd(void)
 {
-  t_my_fd			*buffer;
-
-  if ((buffer = malloc(sizeof(t_my_fd) * 3)) == NULL)
-    return;
-  my_memset(buffer, 0, sizeof(t_my_fd) * 3);
-  my_fdin = buffer;
-  my_fdout = buffer + 1;
-  my_fderr = buffer + 2;
+  my_memset(g_my_fd_buffer, 0, sizeof(g_my_fd_buffer));
+  my_fdin = g_my_fd_buffer;
+  my_fdout = g_my_fd_buffer + 1;
+  my_fderr = g_my_fd_buffer + 2;
   my_fdin->fd = (S_IN);
   my_fdout->fd = (S_OUT);
   my_fderr->fd = (S_ERR);
@@ -38,5 +35,4 @@ LMY_DESTRUCTOR static void	__libmy_close_stdfd(void)
   my_fflush(my_fdin);
   my_fflush(my_fdout);
   my_fflush(my_fderr);
-  free(my_fdin);
 }
--- tmp/lib/my/src/my_printf/pf_format.c
+++ tmp/lib/my/src/my_printf/pf_format.c
@@ -5,7 +5,7 @@
 ** Login   <benjamin.viguier@epitech.eu>
 ** 
 ** Started on  Mon Feb 27 12:21:52 2017 Benjamin Viguier
-** Last update Thu May 18 01:21:47 2017 Benjamin Viguier
+** Last update Thu May 18 01:33:07 2017 Benjamin Viguier
 */
 
 #include "internal.h"
@@ -46,10 +46,17 @@ int		__pf_format(t_pf_data *data, t_pf_prm *prm)
       data->fmt = ((*(prm->save) == '%') ? (prm->save + 1) : prm->save);
       return (0);
     }
-  fct = g_pf_table[prm->type - 'A'];
-  printf("%c %d %d %d %d %s %p %d %p %s\n", prm->type, prm->width, prm->preci, prm->flag, prm->size, prm->pat, fct, prm->type - 'A', prm->myvar.s, prm->myvar.s);
-  if (fct)
-    return (fct(data, prm));
+  if (prm->type >= 'A' && prm->type <= 'z')
+    {
+      fct = g_pf_table[prm->type - 'A'];
+      printf("%c %d %d %d %d %s %p %d %p %s\n", prm->type, prm->width, prm->preci, prm->flag, prm->size, prm->pat, fct, prm->type - 'A', prm->myvar.s, prm->myvar.s);
+      if (fct)
+	return (fct(data, prm));
+    }
+  data->fmt = prm->save;
+  __pf_putchar(data, '%');
+  if (*(data->fmt) == '%')
+    (data->fmt)++;
   return (0);
 }
 
